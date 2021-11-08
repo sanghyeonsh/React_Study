cf629a1e6e918d73ea9158ba1b79bd0c889722d7
sanghyeonsh <ksh3967@naver.com>
2021-11-08 23:30:10 +0900

    1108

--- src/App.js
+++ src/App.js
@@ -9,22 +9,81 @@ function App() {
           Company name
         </div>
         <div className="header_menu">
-          <div>
-            Feature
+          <div className="header_submenu">
+            Features
           </div>
-          <div>
+          <div className="header_submenu">
             Enterprise
           </div>
-          <div>
+          <div className="header_submenu">
             Support
           </div>
-          <div>
+          <div className="header_submenu">
             Pricing
           </div>
           <button className="header_button">
             Sign up
           </button>
         </div>
+      </div>
+      <div className="center">
+        <div className="center_top">
+          <div className="center_pricing">
+            Pricing
+          </div>
+          <div className="center_text">
+            <p>
+              Quickly build an effective pricing table for your potential customers with this
+              <br/>
+              Bootstrap example. It's built with default Bootstrap components and utilities with
+              <br/>
+              little customization.
+            </p>
+          </div>
+        </div>
+        <div className="center_bottom">
+          <div className="card">
+            <div className="card_top">
+              Free
+            </div>
+            <div className="card_bottom">
+              <div className="card_bottom_price">
+                <div className="card_bottom_dollar">
+                  $0
+                </div>
+                <div className="card_bottom_month">
+                  / mo
+                </div>
+              </div>
+              <ul>
+                <li>10 users included</li>
+                <li>2 GB of storage</li>
+                <li>Email support</li>
+                <li>Help center access</li>
+              </ul>
+              <button>
+                Sign up for free
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card_top">
+              Pro
+            </div>
+            <div className="card_bottom">
+
+            </div>
+          </div>
+          <div className="card">
+            <div className="card_top">
+              Enterprise
+            </div>
+            <div className="card_bottom">
+
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
